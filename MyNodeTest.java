0eda0ebe24a2ca9d6f68b0e07642d7e5f2971d37
Ishani9 <57087410+Ishani9@users.noreply.github.com>
2020-10-12 19:59:15 +0530

    Created:  A simple Linked List

---
+++ MyNodeTest.java
@@ -0,0 +1,19 @@
+package com.bl.assignment;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class MyNodeTest {
+
+	@Test
+	public void given3Numbers_WhenLinked_ShouldPassLinkedListTest() { 
+		MyNode<Integer> myFirstNode = new MyNode<> (56);
+		MyNode<Integer> mySecondNode = new MyNode<> (30); 
+		MyNode<Integer> myThirdNode = new MyNode<> (70);
+		myFirstNode.setNext(mySecondNode);
+		mySecondNode.setNext(myThirdNode);
+		boolean result = myFirstNode.getNext().equals(mySecondNode) && mySecondNode.getNext().equals(myThirdNode);
+		Assert.assertTrue(result);
+
+	}
+}
